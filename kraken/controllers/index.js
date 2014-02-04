bafcaa2e0301de96d6499b8bfd79af927c0efdf1
Taylor Ritenour <taylorritenour@berkeley.edu>
2014-02-04 14:00:36 -0800

    cloudinary registration

--- kraken/controllers/index.js
+++ kraken/controllers/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-
 var IndexModel = require('../models/index');
-
+var youtube = require('youtube-api');
 
 module.exports = function (app) {
-
     var model = new IndexModel();
-
-
+    var cloudinary = require('cloudinary'),
+    nconf = require('nconf'),
+    cloudinary_config = nconf.get('cloudinary');
+    cloudinary.config(cloudinary_config);
     app.get('/', function (req, res) {
-        
-        res.render('index', model);
-        
+	cloudinary.uploader.upload("http://cloudinary.com/images/npm_logo.png", function(result) { 
+		console.log(result);
+	    });
+	res.render('index', model);
     });
-
 };
