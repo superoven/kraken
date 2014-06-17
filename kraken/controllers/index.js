65e8935f23bf267ce641ddecb78d739b480f4469
Taylor Ritenour <tritenour@ebay.com>
2014-06-17 11:37:31 -0700

    minor cleanup

--- kraken/controllers/index.js
+++ kraken/controllers/index.js
@@ -4,17 +4,17 @@ var IndexModel = require('../models/index');
 
 module.exports = function (app) {
     var model = new IndexModel();
-
+    
     var server = app.listen(9000);
     var io = require('socket.io').listen(server);
-
+    
     app.get('/', function (req, res) {
-	    res.render('index', model);
-	});
-
+	res.render('index', model);
+    });
+    
     io.sockets.on('connection', function (socket) {
-	    socket.on('mousemove', function (data) {
-		    socket.broadcast.emit('moving', data);
-		});
+	socket.on('mousemove', function (data) {
+	    socket.broadcast.emit('moving', data);
 	});
+    });
 };
--- kraken/public/js/client.js
+++ kraken/public/js/client.js
@@ -94,4 +94,4 @@ $(function() {
 	    ctx.stroke();
 	}
 
-    });
+});
